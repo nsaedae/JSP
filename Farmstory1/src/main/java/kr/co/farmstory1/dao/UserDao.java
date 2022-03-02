3a8fcc4c6b4ce88b05f7ed698131cc949cb106b5
unknown <nsaedae@gmail.com>
2022-03-02 10:49:22 +0900

    2022/03/02 수업예제

--- Farmstory1/src/main/java/kr/co/farmstory1/dao/UserDao.java
+++ Farmstory1/src/main/java/kr/co/farmstory1/dao/UserDao.java
@@ -1,10 +1,12 @@
 package kr.co.farmstory1.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 import kr.co.farmstory1.bean.TermsBean;
+import kr.co.farmstory1.bean.UserBean;
 import kr.co.farmstory1.db.DBConfig;
 import kr.co.farmstory1.db.Sql;
 
@@ -19,7 +21,29 @@ public class UserDao {
 	private UserDao() {}
 	
 	// 기본 CRUD
-	public void insertUser() {}
+	public void insertUser(UserBean user) {
+		
+		try{
+			Connection conn = DBConfig.getInstance().getConnection();
+			PreparedStatement psmt = conn.prepareStatement(Sql.INSERT_USER);
+			psmt.setString(1, user.getUid());
+			psmt.setString(2, user.getPass());
+			psmt.setString(3, user.getName());
+			psmt.setString(4, user.getNick());
+			psmt.setString(5, user.getEmail());
+			psmt.setString(6, user.getHp());
+			psmt.setString(7, user.getZip());
+			psmt.setString(8, user.getAddr1());
+			psmt.setString(9, user.getAddr2());
+			psmt.setString(10, user.getRegip());
+			psmt.executeUpdate();
+			
+			conn.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
 	public TermsBean selectTerms() {
 		
 		TermsBean tb = new TermsBean();
--- Farmstory1/src/main/webapp/user/register.jsp
+++ Farmstory1/src/main/webapp/user/register.jsp
@@ -2,7 +2,7 @@
 <%@ include file="../_header.jsp" %>
 
 <section id="user" class="register">
-    <form action="#" method="POST">
+    <form action="/Farmstory1/user/proc/register.jsp" method="POST">
         <table border="1">
             <caption>사이트 이용정보 입력</caption>
             <tr>
