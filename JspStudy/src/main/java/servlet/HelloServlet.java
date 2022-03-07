34200751e3fecfdc6b65ae4a38dfab3a0fe08e39
unknown <nsaedae@gmail.com>
2022-03-07 12:51:36 +0900

    2022/03/07 수업예제

---
+++ JspStudy/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("HelloServlet init 실행...");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter writer = resp.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<meta charset='utf-8'>");
+		writer.println("<title>HelloServlet</title>");		
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h3>HelloServlet</h3>");
+		writer.println("<p>");
+		writer.println("HelloServlet 컴포넌트 실행완료...<br/>");
+		writer.println("<a href='/JspStudy/Sub8/WelcomeServlet.do'>WelcomeServlet 요청</a>");
+		writer.println("</p>");		
+		writer.println("</body>");		
+		writer.println("</html>");
+		
+		writer.close();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	
+}
---
+++ JspStudy/src/main/webapp/Sub8/1.Servlet.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>1.Servlet</title>
+</head>
+<body>
+	<h3>1.서블릿 실습하기</h3>
+</body>
+</html>
